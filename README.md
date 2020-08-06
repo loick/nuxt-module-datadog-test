b62b322d092dcd0046afe785174b697f258f92a5
Loick Le Digabel <loick.ledigabel@gmail.com>
2020-08-06 12:23:33 +0200

    Add Datadog Module example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nuxt-module-datadog-test
+
+POC related to the issue: https://github.com/nuxt/nuxt.js/pull/7862
---
+++ index.js
@@ -0,0 +1,24 @@
+// Inspired by https://github.com/aaronransley/nuxt-datadog-trace/blob/master/index.js
+module.exports = function DatadogTrace(moduleOptions) {
+  this.nuxt.hook('render:before', () => {
+    const path = require('path')
+    const hostAppPkg = require(path.resolve(this.options.rootDir, 'package.json'))
+    const tracer = require('dd-trace')
+
+    tracer.init({
+      hostname: process.env.DD_TRACE_AGENT_HOSTNAME,
+      port: process.env.DD_TRACE_AGENT_PORT,
+      logInjection: true,
+      runtimeMetrics: true,
+      service: hostAppPkg.name,
+      ...moduleOptions,
+      ...this.options.datadogTrace
+    })
+
+    // Remove the healthcheck from the datadog logs
+    tracer.use('http', {
+      service: process.env.DD_SERVICE_NAME,
+      blacklist: [/\/healthcheck.*/],
+    })
+  })
+}
