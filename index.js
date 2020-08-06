e068b29a68fff7071098514bb055be4b151a02a7
Loick Le Digabel <loick.ledigabel@gmail.com>
2020-08-06 12:25:04 +0200

    Move the package detection out of the hook

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const tracer = require('dd-trace')
 
 // Inspired by https://github.com/aaronransley/nuxt-datadog-trace/blob/master/index.js
 module.exports = function DatadogTrace(moduleOptions) {
+  const hostAppPkg = require(path.resolve(this.options.rootDir, 'package.json'))
+
   this.nuxt.hook('render:before', () => {
-    const hostAppPkg = require(path.resolve(this.options.rootDir, 'package.json'))
 
     tracer.init({
       hostname: process.env.DD_TRACE_AGENT_HOSTNAME,
